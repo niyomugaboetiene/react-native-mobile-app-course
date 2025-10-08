426c39ad06eba21fa8ed44846f5021d3d7bfe467
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-08 07:44:26 +0200

    react-native: learning About View

--- components/view.tsx
+++ components/view.tsx
@@ -2,11 +2,23 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 export default function Hello() {
-    <View style={styles.StyleSheet}>
+    <View style={styles.container}>
         <Text>Top Box</Text>
-        <View style={styles.StyleSheet}>
+        <View style={styles.box}>
             <Text>Inside Box</Text>
         </View>
         <Text>Bottom Box</Text>
     </View>
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f2f2f2',
+  },
+  box: {
+    padding: 20
+  }
+})
