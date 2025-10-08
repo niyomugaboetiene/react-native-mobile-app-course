b4fc5ff559adddbbaa41079e66d0172309fb8785
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-08 10:31:10 +0200

    react-native: learning about <Text>

--- components/text.tsx
+++ components/text.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import { View, Text } from "react-native";
 
 export function Texts() {
-
+  return (
+    <View>
+        <Text style={{ color: 'green', fontSize: '12px' }}>Hello React Native</Text>
+    </View>
+  )
 }
