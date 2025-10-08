e256ff97157ee3105e6dbd294b7e401a2a3fe853
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-08 15:17:54 +0200

    React-native: pasting image from assets folder

--- components/chapter 1/image.tsx
+++ components/chapter 1/image.tsx
@@ -7,7 +7,7 @@ export default function Images() {
                   style={{ width: 100, height: 100 }}           
            />
            {/* on local image */}
-        <Image source={require('./')}/>
+        <Image source={require('../../assets/avataaars.jpg')} />
         </View>
  
     )
