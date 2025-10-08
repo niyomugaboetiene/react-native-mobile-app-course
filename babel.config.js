31cdd65d8178ba5c824a25b63496fe229e3eb12a
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-08 17:55:42 +0200

    React-native: fixing stuff related to taiwlind css

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
+// babel.config.js
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'nativewind/babel',
+    'react-native-reanimated/plugin',
+  ],
 };
+
 // the js compile
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,9 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+// metro.config.js
+const {getDefaultConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const config = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+config.transformer.babelTransformerPath = require.resolve('nativewind/metro');
+config.resolver.assetExts.push('css');
+
+module.exports = config;
