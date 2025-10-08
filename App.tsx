799f9c8bd8b896d96c097fbc3013d9eed3af5558
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-08 20:12:32 +0200

    React-native: fixing stuff related to taiwlind css

--- App.tsx
+++ App.tsx
@@ -17,7 +17,7 @@ export default function App() {
       {/* <Texts /> */}
       {/* <Images /> */}
       {/* <Scroll /> */}
-      <TextInputs />
+      {/* <TextInputs /> */}
     </View>
    )
 }
