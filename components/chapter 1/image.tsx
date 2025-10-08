f6837c6a563780cdfe2b40969bf6afd6dbc23668
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-08 15:02:37 +0200

    React-native: learn about <Image/> tag

--- components/chapter 1/image.tsx
+++ components/chapter 1/image.tsx
@@ -1,5 +1,7 @@
 import { Image } from "react-native";
 
 export default function Images() {
-   
+   <Image source={{ uri: 'https://reactnative.dev/img/tiny_logo.png'}} 
+      style={{ width: 100, height: 100 }}           
+  />
 }
