1761cfcbfbb46c73b74407e4bdea8259cb4a63a6
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-08 15:32:38 +0200

    React-native: learn about <ScrollView> tag

--- components/chapter 1/scrollView.tsx
+++ components/chapter 1/scrollView.tsx
@@ -1,7 +1,7 @@
 // * ScrollView allows you to scroll content vertically or horizontally
 // from is used to create new array 
+import { ScrollView, Text, View } from "react-native";
 
-import { ScrollView, Text } from "react-native";
 export default function Scroll() {
     return (
     <ScrollView
@@ -9,6 +9,10 @@ export default function Scroll() {
     >
        {Array.from({ length: 20}).map((_, i) => (
         <Text key={i}>Item {i + 1}</Text>
+       ))}
+
+       {Array.from({ length: 50}).map((_, i) => (
+        <View key={i} style={{ height: 50, backgroundColor: randomColor() }}/>
        ))}
     </ScrollView>
 )}
