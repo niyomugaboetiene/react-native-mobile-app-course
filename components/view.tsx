b1ea0224a106900239474011e140c523c2e57d22
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-08 07:42:56 +0200

    react-native: learning About View

---
+++ components/view.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Hello() {
+    <View style={styles.StyleSheet}>
+        <Text>Top Box</Text>
+        <View style={styles.StyleSheet}>
+            <Text>Inside Box</Text>
+        </View>
+        <Text>Bottom Box</Text>
+    </View>
+}
