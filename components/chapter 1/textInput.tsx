025152c348dc1af588398dbb3aece12c3d1dfd90
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-08 15:46:58 +0200

    React-native: learn about <TextInput/> tag

--- components/chapter 1/textInput.tsx
+++ components/chapter 1/textInput.tsx
@@ -1,13 +1,36 @@
-import { TextInput } from "react-native";
+import { TextInput, View, StyleSheet, Text } from "react-native";
 import React, { useState} from "react";
 
 export default function TextInputs() {
     const [name, setName] = useState("");
 
     return (
-        <TextInput 
-            placeholder="Enter some thing ..." 
-            onChangeText={setName}
-            />
+        <View style={styles.container}>
+            <TextInput 
+               placeholder="Enter some thing ..." 
+               onChangeText={setName}
+               value={name}
+               style={styles.input}
+        />
+        <Text style={styles.text}>{name}</Text>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'green',
+        padding: 10,
+        width: 50
+    },
+    input: {
+        outlineColor: 'blue',
+        width: 100,
+        padding: 10
+    },
+    text: {
+        fontFamily: 'serif',
+        fontSize: 12,
+        fontStyle: 'italic'
+    }
+});
