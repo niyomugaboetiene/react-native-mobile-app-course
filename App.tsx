02471917f9b5ba36be419be7cc5d66b3b30d1687
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-08 13:19:03 +0200

    React-native: learn about Text tag

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // * View is like div in web is parent component, Text lets you define or add text in your code
 // styleSheet allows you to define css in js instead of css
 import { View, Text, StyleSheet } from "react-native";
-import Hello from "./components/view.tsx";
+// import Hello from "./components/view.tsx";
 import { Texts } from "./components/text.tsx";
 export default function App() {
    return (
